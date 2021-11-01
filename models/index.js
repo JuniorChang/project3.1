79a1e7b5733f8dd161843ea467f0518542358d16
JuniorChang <changjiale@gmail.com>
2021-11-01 12:19:01 +0000

    created cart migration, cart data layer next

--- models/index.js
+++ models/index.js
@@ -18,4 +18,11 @@ const User = bookshelf.model('User', {
     tableName: 'users'
 });
 
-module.exports = { Product, Country, User };
+const CartItem = bookshelf.model('CartItem', {
+    tableName:'cart_items',
+    product() {
+        return this.belongsTo('Product')
+    }
+});
+
+module.exports = { Product, Country, User, CartItem };
